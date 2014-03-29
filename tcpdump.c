509a2d800c4f1b9b2cb31aa1c7e10a26c48b2b93
Brian Ward <bri@aem7.net>
2014-03-28 20:31:01 -0700

    applied debian patch

--- tcpdump.c
+++ tcpdump.c
@@ -67,10 +67,6 @@ static char const GCC_UNUSED rcsid[] =
 
 
 
-
-/* external ref, in case missing in older version */
-extern int pcap_offline_read(void *, int, pcap_handler, u_char *);
-
 /* global pointer, the pcap info header */
 static pcap_t *pcap;
 
@@ -248,7 +244,7 @@ pread_tcpdump(
     int ret;
 
     while (1) {
-	if ((ret = pcap_offline_read(pcap,1,(pcap_handler)callback,0)) != 1) {
+	if ((ret = pcap_dispatch(pcap,1,(pcap_handler)callback,0)) != 1) {
 	    /* prob EOF */
 
 	    if (ret == -1) {
