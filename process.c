044b60df5fb77b53660d3861feb1cf132329ec08
Brian Ward <bri@aem7.net>
2014-04-13 17:09:13 -0700

    removed unused code

--- process.c
+++ process.c
@@ -179,45 +179,6 @@ That will likely confuse the program, so be careful!\n", filename);
             continue;
         }
 
-        /* moved to check_packet_type() (above) */
-#if 0
-	/* quick sanity check, better be an IPv4/v6 packet */
-	if (!PIP_ISV4(pip) && !PIP_ISV6(pip)) {
-	    static Bool warned = FALSE;
-
-	    if (!warned) {
-		fprintf(stderr,
-			"Warning: saw at least one non-ip packet\n");
-		warned = TRUE;
-	    }
-
-	    if (debug)
-		fprintf(stderr,
-			"Skipping packet %lu, not an IPv4/v6 packet (version:%d)\n",
-			state->pnum, IP_V(pip));
-	    continue;
-	}
-
-	/* another sanity check, only understand ETHERNET right now */
-	if (phystype != PHYS_ETHER) {
-	    static int not_ether = 0;
-
-	    ++not_ether;
-	    if (not_ether == 5) {
-		fprintf(stderr,
-			"More non-ethernet packets skipped (last warning)\n");
-		fprintf(stderr, "\n\
-If you'll send me a trace and offer to help, I can add support\n\
-for other packet types, I just don't have a place to test them\n\n");
-	    } else if (not_ether < 5) {
-		fprintf(stderr,
-			"Skipping packet %lu, not an ethernet packet\n",
-			state->pnum);
-	    } /* else, just shut up */
-	    continue;
-	}
-#endif
-
 	/* print the packet, if requested */
 	if (options->printallofem || dump_packet_data) {
 	    printf("Packet %lu\n", state->pnum);
